bc9520c20db8a9b9f3ec702821a97dbd2212c087
Jonas Schmidt <jonaschmidt.en@gmail.com>
2023-02-08 21:56:05 -0600

    Initial Upload - Hypervector Generation Method
    Code to generate variable-dimensional hypervectors for grams 'a' to 'z' and '#' as a space character.

---
+++ hypervector_generation.py
@@ -0,0 +1,43 @@
+'''
+created by Jonas Schmidt on 2/8/2023
+'''
+
+
+import random
+
+# hyper parameters
+hyper_vector_size = 1_000
+n_gram_len = 3
+
+# alphabet dictionary
+alphabet = {'a':[],'b':[],'c':[],'d':[],'e':[],
+            'f':[],'g':[],'h':[],'i':[],'j':[],
+            'k':[],'l':[],'m':[],'n':[],'o':[],
+            'p':[],'q':[],'r':[],'s':[],'t':[],
+            'u':[],'v':[],'w':[],'x':[],'y':[],
+            'z':[],'#':[]}
+
+# keeps track of generated 1's to prove randomness of vector generation
+ratio_track = 0
+
+# generate a hyper vector to assign to each dictionary entry of alphabet
+for letter in alphabet:
+    hypervector = []
+    for d in range(hyper_vector_size):
+        ran = random.random()
+        # 50/50 append a 1 or -1 to hyper_vector:
+        # (could potentially be a way to optimize this)
+        hypervector.append(-1)
+        if ran % 0.5 == ran:
+            hypervector[d] *= (-1)
+            ratio_track += 1
+
+    alphabet.update({letter: hypervector})
+
+# print statements...
+for letter in alphabet:
+    print(letter, ":", alphabet[letter])
+
+print("ratio of all calculated 1's to -1's: ", ratio_track / (len(alphabet) * hyper_vector_size))
+# ...
+
