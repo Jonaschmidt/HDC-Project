098511dfb91eaeb95836a24e0e802d310e317250
Jonas Schmidt <jonaschmidt.en@gmail.com>
2023-02-09 16:41:14 -0600

    Defined Vector Rotation Function, Filters Input
    Filters input sentence,
    +rot

--- hypervector_generation.py
+++ hypervector_generation.py
@@ -5,6 +5,21 @@ created by Jonas Schmidt on 2/8/2023
 import math
 import random
 
+
+# rotate given vector vec by rot_amt
+def rot(vec, rot_amt):
+    vec_size = len(vec)
+
+    rot_vec = vec[rot_amt % vec_size : vec_size]
+    rot_vec.extend(vec[0 : rot_amt % vec_size])
+    return rot_vec
+
+
+# TODO: encodes a dictionary of "n-grams" across a given dictionary lexicon lex
+def encode_n_grams(lex, n_grams):
+    return 0
+
+
 # hyperparameters
 hypervector_size = 1_000
 n_gram_len = 3
@@ -16,11 +31,12 @@ alphabet = {'a':[],'b':[],'c':[],'d':[],'e':[],
             'p':[],'q':[],'r':[],'s':[],'t':[],
             'u':[],'v':[],'w':[],'x':[],'y':[],
             'z':[],'#':[]}
+num_seed_vectors = len(alphabet)
 
 # keeps track of generated 1's to prove randomness of vector generation
 ratio_track = 0
 
-### generating hypervectors for atomic elements
+### generating seed hypervectors for atomic elements
 # generate a hyper vector to assign to each dictionary entry of alphabet
 for letter in alphabet:
     hypervector = []
@@ -34,15 +50,24 @@ for letter in alphabet:
     alphabet.update({letter: hypervector})
 
 # print statements...
+'''
 for letter in alphabet:
     print(letter, ":", alphabet[letter])
 
 print("ratio of all calculated 1's to -1's: ", ratio_track / (len(alphabet) * hypervector_size))
+'''
 # ...
 
 ### generating hypervectors for n-grams
-sentence = input("Enter a sentence to decompose into n-grams: ")
+n_grams = {}
 
+# TODO: filter out punctuation, etc.
+sentence = input("Enter a sentence to decompose into n-grams: ").lower().replace(" ", "#")
+
+# generate a dictionary of n-grams based on input sentence
 for s in range(len(sentence) - n_gram_len + 1):
-    print(sentence[s:s + n_gram_len])
+    curr_gram = sentence[s:s + n_gram_len]
+    n_grams[curr_gram] = []
+
+print(n_grams)
 
