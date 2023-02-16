3add57802341c557bf799ad2e875d841c44adc19
Jonas Schmidt <jonaschmidt.en@gmail.com>
2023-02-15 22:12:26 -0600

    Argument Name Adjustments
    alphabet, lexicon -> "symbol-space"

--- hypervector_generation.py
+++ hypervector_generation.py
@@ -9,33 +9,36 @@ import numpy as np
 import re
 
 
-# generate a hyper vector to assign to each dictionary entry of an alphabet
-#@jit(target='GPU')
-def generate_hypervectors(alphabet, hypervector_size):
-  for letter in alphabet:
-      hypervector = []
-      for d in range(hypervector_size):
-          ran = random.random()
-          # 50/50 append a 1 or -1 to hyper_vector:
-          hypervector.append(-1 + 2 * math.floor(ran + 0.5))
+# generate a hyper vector to assign to each dictionary entry of a symbol-space
+# e.g., generate a hypervector for each entry of the alphabet if symbol_space is the alphabet
+# @jit(target='GPU')
+def generate_hypervectors(symbol_space, hypervector_size):
+    for symbol in symbol_space:
+        hypervector = []
+        for d in range(hypervector_size):
+            ran = random.random()
+            # 50/50 append a 1 or -1 to hyper_vector:
+            hypervector.append(-1 + 2 * math.floor(ran + 0.5))
 
-      alphabet.update({letter: hypervector})
-  return alphabet
+        symbol_space.update({symbol: hypervector})
+    return symbol_space
+
 
 # rotate given vector vec by rot_amt to the left
 # e.g., rot([1,2,3,4], 2) returns [3,4,1,2]
-#@jit(target='GPU')
+# @jit(target='GPU')
 def rot(vec, rot_amt):
     vec_size = len(vec)
 
-    rot_vec = vec[rot_amt % vec_size : vec_size]
-    rot_vec.extend(vec[0 : rot_amt % vec_size])
+    rot_vec = vec[rot_amt % vec_size: vec_size]
+    rot_vec.extend(vec[0: rot_amt % vec_size])
     return rot_vec
 
-# encode all elements of n_grams across lexicon lex
+
+# encode all elements of n_grams across symbol_space
 # (ex. rrT + rH + E, where r represents a rotation operation and T,H,E are elements of an n-gram)
-#@jit(target='GPU')
-def encode_n_grams(lex, n_grams, n_gram_len):
+# @jit(target='GPU')
+def encode_n_grams(symbol_space, n_grams, n_gram_len):
     # for each n-gram in the n_grams dictionary
     for n in n_grams:
         # create / clear a list, mult_vecs, of vectors to multiply
@@ -45,7 +48,7 @@ def encode_n_grams(lex, n_grams, n_gram_len):
             # find the lex hypervector representation of the letter,
             # rotate the vector based on its position in the n-gram,
             # append this rotated vector to mult_vecs
-            mult_vecs.append(np.array(rot(lex[n[i]], n_gram_len - i - 1)))
+            mult_vecs.append(np.array(rot(symbol_space[n[i]], n_gram_len - i - 1)))
 
         # for each hypervector in mult_vecs, produce a Hadamard product,
         # define this n-gram by this Hadamard product
@@ -56,16 +59,21 @@ def encode_n_grams(lex, n_grams, n_gram_len):
         n_grams[n] = list(mult_vecs[-1])
         mult_vecs.clear()
 
+
 # scrubs a given string as per rules described below:
 '''
 input scrubbing rules:
 replace space characters and punctuation with "#", 
-replace any resulting duplicate hashmarks ("##", "###", "####", etc.) with single ("#")
+replace resulting duplicate hashmarks ("##", "###", "####", etc.) with single ("#")
 '''
-#@jit(target='GPU')
+
+
+# TODO: improve duplicate hash replacement with regex
+# @jit(target='GPU')
 def scrub(sentence):
     sentence = re.sub(r'[^\w\s]', "#", sentence)
     sentence = sentence.replace(" ", "#")
     sentence = sentence.replace("###", "#")
     return sentence.replace("##", "#")
 
+  
