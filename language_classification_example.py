d56e5738b1807deb5d8aad7d1c1826238f1ba102
Jonas Schmidt <jonaschmidt.en@gmail.com>
2023-03-29 16:16:29 -0500

    Update language_classification_example.py

--- language_classification_example.py
+++ language_classification_example.py
@@ -1,9 +1,13 @@
 '''
 created by Jonas Schmidt on 3/17/2023
 '''
+# TODO: move binarize from hgen to vcomp
+# TODO: binarize returns [[...]], change to [...]
+
 import numpy as np
 import hypervector_generation as hgen
 import vector_comparison as vcomp
+import tatoeba_access as ta
 
 # hyperparameters
 hypervector_size = 8_000
@@ -29,61 +33,57 @@ TUR_CLASS = np.zeros(hypervector_size)
 # train
 print("training...")
 
-eng = hgen.scrub("In 1978 Johnson was awarded an American Institute of Architects Gold Medal. In 1979 he became the "
-                 "first recipient of the Pritzker Architecture Prize the most prestigious international architectural"
-                 " award.")
-tur = hgen.scrub("Tsutinalar (İngilizce: Tsuut'ina): Kanada'da Alberta bölgesinde Calgary'de yaşarlar. Tek başına "
-                 "grup oluştururlar ve Pasifik ve Güney Atabaskları ile antik yakınlıklar göstermiştir.")
+train_set, test_set = ta.load_data(train_num, test_num)
 
-### Train on English paragraph
-eng = hgen.scrub(eng)
-eng = hgen.decompose_sequence(eng, n_gram_len)
+for i in range(train_num):
+    curr_label = train_set[i][0]
+    curr_seq = train_set[i][1]
 
-# encode the n-grams
-for n in enumerate(eng):
-    eng[n[0]] = hgen.encode_n_gram(alphabet, n[1])
-
-# accumulate the n-grams
-acc = np.array(hgen.sum_vec(eng))
-
-ENG_CLASS = hgen.sum_vec([ENG_CLASS, acc])[0]
-
-### Train on Turkish paragraph
-tur = hgen.scrub(tur)
-tur = hgen.decompose_sequence(tur, n_gram_len)
-
-# encode the n-grams
-for n in enumerate(tur):
-    tur[n[0]] = hgen.encode_n_gram(alphabet, n[1])
-
-# accumulate the n-grams
-acc = np.array(hgen.sum_vec(tur))
-
-TUR_CLASS = hgen.sum_vec([TUR_CLASS, acc])[0]
-
-#print("Cosine sim:", vcomp.cosine_similarity(ENG_CLASS, TUR_CLASS))
-
-user_in = hgen.scrub(input("Enter an English or Turkish sentence, or 'exit' to exit: "))
-
-while user_in != 'exit':
-    user_in = hgen.decompose_sequence(user_in, n_gram_len)
-    user_in_vec = np.zeros(hypervector_size)
+    # TODO: make this an np array?
+    vec = []
 
     # encode the n-grams
-    for n in enumerate(user_in):
-        user_in[n[0]] = hgen.encode_n_gram(alphabet, n[1])
+    for n in enumerate(curr_seq):
+        vec.append(hgen.encode_n_gram(alphabet, n[1]))
 
     # accumulate the n-grams
-    acc = np.array(hgen.sum_vec(user_in))
+    acc = np.array(hgen.sum_vec(vec))
 
-    user_in_vec = hgen.sum_vec([user_in_vec, acc])[0]
-
-    if vcomp.cosine_similarity(ENG_CLASS, user_in_vec) > vcomp.cosine_similarity(TUR_CLASS, user_in_vec):
-        prediction = "English"
+    if curr_label == "tur":
+        TUR_CLASS = hgen.sum_vec([acc, TUR_CLASS])
     else:
-        prediction = "Turkish"
+        ENG_CLASS = hgen.sum_vec([acc, ENG_CLASS])
 
-    print("Prediction:", prediction, "\n")
+TUR_CLASS = TUR_CLASS[0]
+ENG_CLASS = ENG_CLASS[0]
 
-    user_in = hgen.scrub(input("Enter an English or Turkish sentence, or 'exit' to exit: "))
+print("TUR_CLASS =", TUR_CLASS)
+print("ENG_CLASS =", ENG_CLASS)
+
+hgen.binarize(TUR_CLASS)
+hgen.binarize(ENG_CLASS)
+
+print(vcomp.cosine_similarity(TUR_CLASS, ENG_CLASS))
+
+print("testing...")
+
+accu = 0
+
+for seq in test_set:
+    prediction = "tur"
+
+    n_gram_list = hgen.decompose_sequence(seq[1], n_gram_len)
+
+    test_hypervector = np.zeros(hypervector_size)
+
+    for i in n_gram_list:
+        test_hypervector = hgen.sum_vec([test_hypervector, hgen.encode_n_gram(alphabet, i)])
+
+    if vcomp.cosine_similarity(test_hypervector, TUR_CLASS) > vcomp.cosine_similarity(test_hypervector, ENG_CLASS):
+        prediction = "eng"
+
+    if prediction == seq[0]:
+        accu += 1
+
+print("Accuracy (%):", 100 * accu / test_num)
 
